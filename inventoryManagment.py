df0de80993e87f69d75b65e9d1179b5a0afbd240
Jobey Farias <JobeyFarias@gmail.com>
2023-04-13 09:15:34 -0500

    Finished button Placements for products

--- inventoryManagment.py
+++ inventoryManagment.py
@@ -19,7 +19,7 @@ class App(customtkinter.CTk):
         self.geometry("1100x580")
         self.title("Inventory Management System")
 # sidebar Frame
-        self.sidebar_frame = ctk.CTkFrame(self, width = 150, corner_radius=0)
+        self.sidebar_frame = ctk.CTkFrame(self, width = 150, corner_radius=0, border_color = "gray30", border_width = 1)
         self.sidebar_frame.pack(side = 'left', fill = 'y')
 
 # Main Frame
@@ -40,8 +40,8 @@ class App(customtkinter.CTk):
         self.appearance_mode_label = customtkinter.CTkLabel(self.sidebar_frame, text="Appearance Mode:", anchor="w").pack(pady = 30)
         self.appearance_mode_optionemenu = customtkinter.CTkOptionMenu(self.sidebar_frame, values=["Dark", "Light", "System"],command=self.change_appearance_mode_event).pack()
 
-        self.scaling_label = customtkinter.CTkLabel(self.sidebar_frame, text="UI Scaling:", anchor="w").pack(pady = 30)
-        self.scaling_optionemenu = customtkinter.CTkOptionMenu(self.sidebar_frame, values=["100%", "110%", "120%", "90%", "80%"],command=self.change_scaling_event).pack()
+        #self.scaling_label = customtkinter.CTkLabel(self.sidebar_frame, text="UI Scaling:", anchor="w").pack(pady = 30)
+        #self.scaling_optionemenu = customtkinter.CTkOptionMenu(self.sidebar_frame, values=["100%", "110%", "120%", "90%", "80%"],command=self.change_scaling_event).pack()
         
         
         
@@ -51,10 +51,18 @@ class App(customtkinter.CTk):
 
     def sidebar_products_event(self):
         self.clear_frame(self.main_frame)
-        self.drinks_label = customtkinter.CTkLabel(self.main_frame, text = "Drinks",font=ctk.CTkFont(size=20, weight="bold")).pack(side = "top", padx = 10)
-        self.drinks_button = customtkinter.CTkButton(self.main_frame, text = "Drinks", width = 150, height =150).pack(side = "top", pady = 10, padx = 10)
-        self.snacks_label = customtkinter.CTkLabel(self.main_frame, text = "Snacks",font=ctk.CTkFont(size=20, weight="bold")).pack(side = "top")
-        self.snacks_button = customtkinter.CTkButton(self.main_frame, text = "Snacks").pack(side = "top", pady = 10, padx = 10)
+        self.drinks_label = customtkinter.CTkLabel(self.main_frame, text = "Drinks",font=ctk.CTkFont(size=20, weight="bold"))
+        self.drinks_label.place(x = 170, y = 150)
+        self.drinks_button = customtkinter.CTkButton(self.main_frame, text = "Drinks", width = 200, height =100)
+        self.drinks_button.place(x = 100, y = 200) 
+        self.snacks_label = customtkinter.CTkLabel(self.main_frame, text = "Snacks",font=ctk.CTkFont(size=20, weight="bold"))
+        self.snacks_label.place(x = 445, y = 150)
+        self.snacks_button = customtkinter.CTkButton(self.main_frame, text = "Snacks", width = 200, height = 100)
+        self.snacks_button.place(x = 375, y = 200)
+        self.items_label = customtkinter.CTkLabel(self.main_frame, text = "Items",font=ctk.CTkFont(size=20, weight="bold"))
+        self.items_label.place(x= 725, y = 150)
+        self.items_button = customtkinter.CTkButton(self.main_frame, text = "Items",width = 200, height = 100 )
+        self.items_button.place(x = 650, y = 200)
         
 
     def sidebar_suppliers_event(self):
@@ -79,6 +87,8 @@ class App(customtkinter.CTk):
     def change_scaling_event(self, new_scaling: str):
         new_scaling_float = int(new_scaling.replace("%", "")) / 100
         ctk.set_widget_scaling(new_scaling_float)
+
+    
 
 if __name__ == "__main__":
     app = App()
